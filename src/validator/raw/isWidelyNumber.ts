b6f97b00a46dbf8ec71ae38c1a7fb40661e58dda
moretall <seongho4996@naver.com>
2022-04-10 20:52:16 +0900

    fix(isWidelyNumber): null or undefined check 추가

--- src/validator/raw/isWidelyNumber.ts
+++ src/validator/raw/isWidelyNumber.ts
@@ -1,5 +1,12 @@
+import { isNullOrUndefined } from './isNullOrUndefined';
+
 export const isWidelyNumber = (value: any): value is number => {
+  if (isNullOrUndefined(value)) {
+    return false;
+  }
+
   const parsedValue = Number(value);
+
   if (Number.isNaN(parsedValue)) {
     return false;
   }
